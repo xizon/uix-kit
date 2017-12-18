6133934e28cf80042fedcffc114ac84da60cdbc5
Chuckie Chang <546007561@qq.com>
2017-12-18 18:45:21 +0800

    Update

--- assets/js/uix-kit.js
+++ assets/js/uix-kit.js
@@ -1907,14 +1907,14 @@ theme = ( function ( theme, $, window, document ) {
     var pageLoaded = function() {
 		
 		
-		var $window      = $( window ),
-			windowWidth  = $window.width(),
-			windowHeight = $window.height(),
-			$timeline    = $( '.list-timeline-container-outer.horizontal > .list-timeline-container' ),
-			$item        = $timeline.find( '.list-timeline-item' );
+		var $window          = $( window ),
+			windowWidth      = $window.width(),
+			windowHeight     = $window.height(),
+			$timeline        = $( '.list-timeline-container-outer.horizontal > .list-timeline-container' ),
+			$item            = $timeline.find( '.list-timeline-item' );
 				
 
-		//Timeline initialize
+		//--------  Timeline initialize
 		if ( windowWidth > 768 ) {
 		    timelineInit();
 		}
@@ -1942,18 +1942,94 @@ theme = ( function ( theme, $, window, document ) {
 
 			$timeline.css( 'width', totalWidth + 'px' );
 			
+			//Use the wheel to control the timeline
+			/*
 			$( '.list-timeline-container-outer.horizontal' ).on( 'mousewheel', function( event, delta) {
 				this.scrollLeft -= (delta * 10);
 				event.preventDefault();
 			});	
+			*/
 	
 			
 		}
-			
 		
 		
+		//--------  Timeline Event
+		$( '.list-timeline-container-outer-wrapper.horizontal' ).each( function()  {
 			
-	
+			
+			var $this = $( this );
+			
+			$this.css( 'height', $this.height() - 17 + 'px' ); //Scrollbar width is 17px by default
+
+
+			$this.find( '.timeline-prev' ).on( 'click', function( e ) {
+				e.preventDefault();
+				timelinePrev( $this );
+				return false;
+			});
+
+			$this.find( '.timeline-next' ).on( 'click', function( e ) {
+				e.preventDefault();
+				timelineNext( $this );
+				return false;
+			});
+
+			
+		});
+		
+		
+		function timelinePrev( obj ) {
+			var	itemTotal = obj.find( '.list-timeline-item' ).length,
+				tNav    = obj.find( '.list-timeline-item' ),
+				tNavW   = tNav.width(),
+				tLoop   = false;
+			
+			var curIndex = obj.find( '.list-timeline-item.active' ).index(),
+				tarIndex = ( curIndex >= 0  ) ? curIndex-1 : 0;
+
+			//loop the items
+			if ( tLoop ) {
+				if ( tarIndex < 0 ) tarIndex = itemTotal-1;
+			} else {
+				if ( tarIndex < 0 ) tarIndex = 0;
+			}
+			
+			
+			tNav.removeClass( 'active' );
+			obj.find( '.list-timeline-item:eq('+tarIndex+')' ).addClass( 'active' );
+
+			//scroll left
+			obj.find( '.list-timeline-container-outer.horizontal' ).animate({scrollLeft: tarIndex * tNavW }, 300 );	
+		}
+
+
+		function timelineNext( obj ) {
+			var	itemTotal = obj.find( '.list-timeline-item' ).length,
+				tNav    = obj.find( '.list-timeline-item' ),
+				tNavW   = tNav.width(),
+				tLoop   = false;
+			
+			var curIndex = obj.find( '.list-timeline-item.active' ).index(),
+				tarIndex = ( curIndex < itemTotal  ) ? curIndex+1 : itemTotal-1;
+
+			//loop the items
+			if ( tLoop ) {
+				if ( tarIndex == itemTotal ) tarIndex = 0;
+			} else {
+				if ( tarIndex > itemTotal-1 ) tarIndex = itemTotal-1;
+			}
+			
+			tNav.removeClass( 'active' );
+			obj.find( '.list-timeline-item:eq('+tarIndex+')' ).addClass( 'active' );
+			
+
+			//scroll right
+			obj.find( '.list-timeline-container-outer.horizontal' ).animate({scrollLeft: tarIndex * tNavW }, 300 );	
+		}
+
+
+
 
     };
 
