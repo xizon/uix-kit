072caab41fa5f666764be7ff2c7a2f356492beef
Chuckie Chang <546007561@qq.com>
2018-01-07 00:12:24 +0800

    Upgraded component for tabs

--- _src/js-components/14.tabs.js
+++ _src/js-components/14.tabs.js
@@ -18,11 +18,8 @@ theme = ( function ( theme, $, window, document ) {
 				ulWidth           = $this.data( 'width' ),
 				fullwidth         = $this.data( 'fullwidth' ),
 				rotation          = $this.data( 'rotation' ),
-				rotationPathLen   = $this.data( 'rotation-path-len' ),
-				rotationWrapper   = $this.data( 'rotation-wrapper' ),
-				rotationReverse   = $this.data( 'rotation-reverse' ),
-				rotationMove      = $this.data( 'rotation-move' ),
-				rotationAlternate = $this.data( 'rotation-alternate' ),
+				rotationRadius    = $this.data( 'rotation-radius' ),
+				rotationWapperDeg = $this.data( 'rotation-wrapper-angle' ),
 				
 				tabBoxID          = id,
 				isNumeric         = /^[-+]?(\d+|\d+\.\d*|\d*\.\d+)$/;
@@ -31,30 +28,15 @@ theme = ( function ( theme, $, window, document ) {
 				$li.css( 'width', ( 100 / liNum ) + '%' );
 			}
 			
-			if( typeof rotationReverse === typeof undefined ) {
-				rotationReverse = false;
-			}			
-			if( typeof rotationAlternate === typeof undefined ) {
-				rotationAlternate = false;
-			}		
+					
 			
 			if( typeof rotation === typeof undefined ) {
 				rotation = false;
 			}	
 			
-			if( typeof rotationPathLen === typeof undefined ) {
-				rotationPathLen = 360;
-			}		
 			
-			if( typeof rotationWrapper === typeof undefined ) {
-				rotationWrapper = 0;
-			}		
-			
-			if( typeof rotationReverse === typeof undefined ) {
-				rotationReverse = false;
-			}	
-			if( typeof rotationMove === typeof undefined ) {
-				rotationMove = 0;
+			if( typeof rotationWapperDeg === typeof undefined ) {
+				rotationWapperDeg = 0;
 			}		
 			
 			
@@ -71,13 +53,66 @@ theme = ( function ( theme, $, window, document ) {
 			
 			// Tab Rotation Effect
 			if ( rotation ) {
-			
 				
-				//reversing the order of child elements
-				if ( rotationReverse ) $this.find( 'ul' ).html( $this.find( 'ul > li' ).get().reverse() );
+				var increase   = Math.PI * 2 / liNum,
+					radius     = rotationRadius,
+					angle      = 0;
 				
-				initLiPos( $this.find( 'ul' ), rotationPathLen, rotationWrapper, false );
+				//Initialize button position
+				$this.find( 'ul' ).css({ 
+							'-webkit-transform' : 'rotate('+ parseFloat( rotationWapperDeg ) +'deg)',
+							'-ms-transform'     : 'rotate('+ parseFloat( rotationWapperDeg ) +'deg)',
+							'transform'         : 'rotate('+ parseFloat( rotationWapperDeg ) +'deg)'
+						})
+						.find( '> li' )
+						.css({ 
+								'-webkit-transform' : 'rotate('+ -parseFloat( rotationWapperDeg )+'deg)',
+								'-ms-transform'     : 'rotate('+ -parseFloat( rotationWapperDeg )+'deg)',
+								'transform'         : 'rotate('+ -parseFloat( rotationWapperDeg )+'deg)'
+							});
+				
+				
+				$li.each( function( index ) {
+					$( this ).css( {
+						'left'              : Math.cos( - Math.PI / 2 + index * increase) * radius + 'px',
+						'top'               : Math.sin( - Math.PI / 2 + index * increase) * radius + 'px'
+					} );
+					
 
+					
+					$( this ).on( 'click', function( e ) {
+						
+						var n        = $(this).index(),
+							endAngle = n % liNum * increase; 
+
+
+						( function turn() {
+							if (Math.abs(endAngle - angle) > 1 / 8) {
+								var sign = endAngle > angle ? 1 : -1;
+								angle = angle + sign / 8;
+								setTimeout(turn, 20);
+							} else {
+								angle = endAngle;
+							}
+
+
+							$li.each( function( index ) {
+								$( this ).css( {
+									'left'        : Math.cos( - Math.PI / 2 + index * increase - angle) * radius + 'px',
+									'top'         : Math.sin( - Math.PI / 2 + index * increase - angle) * radius + 'px'
+								} );
+
+							});	
+
+
+						})();	
+						
+					});
+					
+				});	
+				
+
+				
 			}
 			
 			
@@ -94,112 +129,14 @@ theme = ( function ( theme, $, window, document ) {
 				$( this ).addClass( 'active' );
 				$( '#' + tabID ).addClass( 'active' );
 				
-
-				// Alternate elements
-				if ( rotationAlternate ) {
-					var beforeCur   = $( this ).nextAll(),
-						afterCur    = $( this ).prevAll();
-
-					
-					if ( beforeCur.length > 0 ) {
-						beforeCur.remove();
-						$this.find( 'ul' ).prepend( beforeCur );	
-						
-					}
-
-					if ( afterCur.length > 0 ) {
-						//afterCur.remove();
-						//$this.find( 'ul' ).append( afterCur );	
-					}			
-				}
-
-
-				
-				// Tab Rotation Effect
-				if ( rotation ) {
-					
-					initLiPos( $this.find( 'ul' ), rotationPathLen, rotationWrapper, index * rotationMove );
-
-				}
 				
 				return false;
 				
 				
 			});
 			
-			// Alternate elements
+			// Init
 			$this.find( 'ul > li.active' ).trigger( 'click' );
-			
-			
-			// Initialize li position
-			function initLiPos( liWrapper, pathLen, degWrapper, clickDeg ) {
-				
-				var liArr      = liWrapper.find( 'li' ),
-					angleStart = -360;
-				
-			
-				if ( !isNumeric.test( clickDeg ) ) {
-					clickDeg = 0;	
-				}
-				
-				if ( rotationAlternate ) {
-					clickDeg = 0;
-				}
-				
-				
-
-					
-				liWrapper.css({ 
-							'-webkit-transform' : 'rotate('+ parseFloat( degWrapper - clickDeg ) +'deg)',
-							'-ms-transform'     : 'rotate('+ parseFloat( degWrapper - clickDeg ) +'deg)',
-							'transform'         : 'rotate('+ parseFloat( degWrapper - clickDeg ) +'deg)'
-						});
-
-		
-
-				//Re-arrange elements
-				for( var i = 0; i < liArr.length; i++ ) {
-
-					var deg = i * parseFloat( pathLen / liArr.length );
-					
-					// Rotate animation
-					$( { deg: angleStart } ).animate( { deg: deg }, {
-						step: function( now ) {
-
-
-
-							if ( !isNumeric.test( clickDeg ) ) {
-								clickDeg = 0;	
-							}
-
-							$( liArr[i] )
-							   .css({ 
-										'-webkit-transform' : 'rotate('+parseFloat( now )+'deg)',
-										'-ms-transform'     : 'rotate('+parseFloat( now )+'deg)',
-										'transform'         : 'rotate('+parseFloat( now )+'deg)'
-									})
-							   .find( 'a' )
-							   .css({ 
-										'-webkit-transform' : 'rotate('+ -parseFloat( now - clickDeg + degWrapper )+'deg)',
-										'-ms-transform'     : 'rotate('+ -parseFloat( now - clickDeg + degWrapper )+'deg)',
-										'transform'         : 'rotate('+ -parseFloat( now - clickDeg + degWrapper )+'deg)'
-									});
-
-
-
-						}, duration: 0 });
-
-				}
-
-			
-				
-			
-			}
-			
-			
-
-			
-			
 				
 			
 		});
