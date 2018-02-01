00efac8e85d380ba22f9796ca9999106440f6414
Chuckie Chang <546007561@qq.com>
2018-02-01 09:36:21 +0800

    Update

--- examples/assets/js/uix-kit.js
+++ examples/assets/js/uix-kit.js
@@ -1166,6 +1166,80 @@ theme = ( function ( theme, $, window, document ) {
 
 /*! 
  *************************************
+ * Form
+ *************************************
+ */
+theme = ( function ( theme, $, window, document ) {
+    'use strict';
+   
+   
+    var documentReady = function( $ ) {
+		
+		/*--- Input File ----*/
+		$( '.controls-file-container' ).each( function()  {
+			var fileInput  = $( this ).find( 'input[type="file"]' ),
+				fileBtn    = $( this ).find( '.controls-file-trigger' ),
+				filePath   = $( this ).next( '.controls-file-return' );
+			
+			fileBtn.on( 'click', function() {
+				fileInput.focusin();
+			});	
+			
+			fileInput.on( 'change', function() {
+				filePath.text( $( this ).val() );
+			});	
+			
+		});
+
+		/*--- Hover Effect ----*/
+		$( '.float-label' ).each( function(){
+			
+			var $this = $( this );
+			
+			// on focus add cladd active to label
+			$this.focus( function() {
+				$this.next().addClass( 'active' );
+			});
+			//on blur check field and remove class if needed
+			$this.blur( function() {
+				if( $this.val() === '' || $this.val() === 'blank') {
+					$this.next().removeClass();
+				}
+			});
+			
+			// if exist cookie value
+			if( $this.val() != '' && $this.val() != 'blank') { 
+			    $this.next().addClass( 'active' );
+			}
+			
+		});
+		
+		$( '.wp-search-submit' ).on( 'click', function() {
+			$( this ).parent().parent( 'form' ).submit();
+		});
+		
+		
+		
+		/*--- Input Validation ----*/
+		//Using the jQuery Validation Plugin to check your form
+		
+		
+	};
+	
+		
+    theme.form = {
+        documentReady : documentReady        
+    };
+
+    theme.components.documentReady.push( documentReady );
+    return theme;
+
+}( theme, jQuery, window, document ) );
+
+
+
+/*! 
+ *************************************
  * Gallery
  *************************************
  */
@@ -1259,80 +1333,6 @@ theme = ( function ( theme, $, window, document ) {
 	
 		
     theme.gallery = {
-        documentReady : documentReady        
-    };
-
-    theme.components.documentReady.push( documentReady );
-    return theme;
-
-}( theme, jQuery, window, document ) );
-
-
-
-/*! 
- *************************************
- * Form
- *************************************
- */
-theme = ( function ( theme, $, window, document ) {
-    'use strict';
-   
-   
-    var documentReady = function( $ ) {
-		
-		/*--- Input File ----*/
-		$( '.controls-file-container' ).each( function()  {
-			var fileInput  = $( this ).find( 'input[type="file"]' ),
-				fileBtn    = $( this ).find( '.controls-file-trigger' ),
-				filePath   = $( this ).next( '.controls-file-return' );
-			
-			fileBtn.on( 'click', function() {
-				fileInput.focusin();
-			});	
-			
-			fileInput.on( 'change', function() {
-				filePath.text( $( this ).val() );
-			});	
-			
-		});
-
-		/*--- Hover Effect ----*/
-		$( '.float-label' ).each( function(){
-			
-			var $this = $( this );
-			
-			// on focus add cladd active to label
-			$this.focus( function() {
-				$this.next().addClass( 'active' );
-			});
-			//on blur check field and remove class if needed
-			$this.blur( function() {
-				if( $this.val() === '' || $this.val() === 'blank') {
-					$this.next().removeClass();
-				}
-			});
-			
-			// if exist cookie value
-			if( $this.val() != '' && $this.val() != 'blank') { 
-			    $this.next().addClass( 'active' );
-			}
-			
-		});
-		
-		$( '.wp-search-submit' ).on( 'click', function() {
-			$( this ).parent().parent( 'form' ).submit();
-		});
-		
-		
-		
-		/*--- Input Validation ----*/
-		//Using the jQuery Validation Plugin to check your form
-		
-		
-	};
-	
-		
-    theme.form = {
         documentReady : documentReady        
     };
 
