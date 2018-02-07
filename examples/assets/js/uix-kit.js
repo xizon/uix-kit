01a5c4b7d0d4a82280f3441ccdb05bbcaac98e8f
Chuckie Chang <546007561@qq.com>
2018-02-07 10:55:14 +0800

    Update

--- examples/assets/js/uix-kit.js
+++ examples/assets/js/uix-kit.js
@@ -6,7 +6,7 @@
  * 
  * ## Project Name        :  Uix Kit
  * ## Description         :  Free Responsive HTML5 UI Kit for Fast Web Design Based On Bootstrap
- * ## Version             :  1.0.0
+ * ## Version             :  1.0.2
  * ## Last Update         :  February 7, 2018
  * ## Created             :  by UIUX Lab (https://uiux.cc)
  * ## Contact Us          :  uiuxlab@gmail.com
@@ -2949,6 +2949,86 @@ theme = ( function ( theme, $, window, document ) {
 
 }( theme, jQuery, window, document ) );
 
+
+/*! 
+ *************************************
+ * Sticky Elements 
+ *************************************
+ */
+theme = ( function ( theme, $, window, document ) {
+    'use strict';
+    
+    var pageLoaded = function() {
+		
+		var $window      = $( window ),
+			windowWidth  = $window.width(),
+			windowHeight = $window.height(),
+			topSpacing   = $( '.header-area' ).outerHeight( true ) + 10;
+		
+		
+		$window.on( 'scroll touchmove', function() {
+
+			var scrollTop   = $window.scrollTop(),
+				dynamicTop  = parseFloat( scrollTop + $window.height() ),
+				targetTop   = parseFloat( $( document ).height() - 200 );
+
+			//Detecting when user scrolls to bottom of div
+			if ( dynamicTop >= targetTop ) {
+				
+				
+				$( '.stick-widget.sticky' )
+					  .css( {
+						  'top'  : parseFloat( topSpacing - (dynamicTop - targetTop) ) + 'px'
+					  } );
+				
+			}
+
+
+		});	
+
+		$( '.stick-widget' ).each( function()  {
+			
+			var $this      = $( this ),
+				oWIdth     = $this.width();
+			
+			
+			var	waypoints = $this.waypoint({
+
+			  handler: function( direction ) {
+				  
+				  $this
+					  .toggleClass( 'sticky', direction === 'down' )
+					  .css( {
+						  'width': oWIdth + 'px',
+						  'top'  : topSpacing + 'px'
+					  } );
+				  
+				
+
+			  },
+
+			  offset: topSpacing
+
+			});	
+		});
+
+
+		
+    };
+
+    theme.stickyElements = {
+        pageLoaded : pageLoaded        
+    };
+
+    theme.components.pageLoaded.push( pageLoaded );
+    return theme;
+
+}( theme, jQuery, window, document ) );
+
+
+
+
+
 /*! 
  *************************************
  * Slideshow ( with custom flexslider )
@@ -3471,86 +3551,6 @@ theme = ( function ( theme, $, window, document ) {
     return theme;
 
 }( theme, jQuery, window, document ) );
-
-
-
-
-/*! 
- *************************************
- * Sticky Elements 
- *************************************
- */
-theme = ( function ( theme, $, window, document ) {
-    'use strict';
-    
-    var pageLoaded = function() {
-		
-		var $window      = $( window ),
-			windowWidth  = $window.width(),
-			windowHeight = $window.height(),
-			topSpacing   = $( '.header-area' ).outerHeight( true ) + 10;
-		
-		
-		$window.on( 'scroll touchmove', function() {
-
-			var scrollTop   = $window.scrollTop(),
-				dynamicTop  = parseFloat( scrollTop + $window.height() ),
-				targetTop   = parseFloat( $( document ).height() - 200 );
-
-			//Detecting when user scrolls to bottom of div
-			if ( dynamicTop >= targetTop ) {
-				
-				
-				$( '.stick-widget.sticky' )
-					  .css( {
-						  'top'  : parseFloat( topSpacing - (dynamicTop - targetTop) ) + 'px'
-					  } );
-				
-			}
-
-
-		});	
-
-		$( '.stick-widget' ).each( function()  {
-			
-			var $this      = $( this ),
-				oWIdth     = $this.width();
-			
-			
-			var	waypoints = $this.waypoint({
-
-			  handler: function( direction ) {
-				  
-				  $this
-					  .toggleClass( 'sticky', direction === 'down' )
-					  .css( {
-						  'width': oWIdth + 'px',
-						  'top'  : topSpacing + 'px'
-					  } );
-				  
-				
-
-			  },
-
-			  offset: topSpacing
-
-			});	
-		});
-
-
-		
-    };
-
-    theme.stickyElements = {
-        pageLoaded : pageLoaded        
-    };
-
-    theme.components.pageLoaded.push( pageLoaded );
-    return theme;
-
-}( theme, jQuery, window, document ) );
-
-
 
 
 
