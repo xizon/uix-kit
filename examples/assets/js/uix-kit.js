473241541c4f53669535f5a6a10bfff1d440f247
Chuckie Chang <546007561@qq.com>
2018-03-20 10:51:28 +0800

    Update

--- examples/assets/js/uix-kit.js
+++ examples/assets/js/uix-kit.js
@@ -3852,6 +3852,10 @@ theme = ( function ( theme, $, window, document ) {
    
     var pageLoaded = function() {
 		
+		
+		//Prevent this module from loading in other pages
+		if ( !$( 'body' ).hasClass( 'page-form-progress-eff' ) ) return false;
+		
 
 		var $progressBar   = $( '.custom-form-progress progress' ),
 			$formTarget    = $( '.custom-form-progress-target' ),
@@ -4948,6 +4952,93 @@ theme = ( function ( theme, $, window, document ) {
 
 
 
+
+/*! 
+ *************************************
+ * Mousewheel Interaction
+ *************************************
+ */
+theme = ( function ( theme, $, window, document ) {
+    'use strict';
+   
+   
+    var documentReady = function( $ ){
+		
+		
+		//Prevent this module from loading in other pages
+		if ( !$( 'body' ).hasClass( 'page-mousewheel-eff' ) ) return false;
+		
+		
+	    //Determine the direction of a jQuery scroll event
+		//Fix an issue for mousewheel event is too fast.
+		var mousewheelTrigger = true,
+			scrollCount       = 0;
+		
+		$( window ).on( 'mousewheel', function( event ) { 
+
+			if ( mousewheelTrigger ) {
+
+				if( event.originalEvent.wheelDelta < 0) {
+					//scroll down
+					$( '#demo-mousewheel-interaction-status' ).text( 'Direction: down, Total: ' + scrollCount );
+
+					scrollCount++;
+					
+					//Prohibited scrolling trigger
+					mousewheelTrigger = false;
+					
+					//Do something
+					customMouseHandle();		
+					
+
+				} else {
+					//scroll up
+					$( '#demo-mousewheel-interaction-status' ).text( 'Direction: up, Total: ' + scrollCount );
+
+					scrollCount++;
+					
+					//Prohibited scrolling trigger
+					mousewheelTrigger = false;
+					
+					//Do something
+					customMouseHandle();
+
+				}	
+
+			}
+			
+
+			//prevent page fom scrolling
+			//return false;
+
+		});
+	
+		
+		
+
+		function customMouseHandle() {
+			
+			//Reset scrolling trigger
+			setTimeout( function() {
+				mousewheelTrigger = true;	
+			}, 1500 );
+			
+			
+		}
+
+		
+	};
+		
+      
+    theme.mousewheelInteraction = {
+        documentReady : documentReady        
+    };  
+    theme.components.documentReady.push( documentReady );
+    return theme;
+
+}( theme, jQuery, window, document ) );
+
+
 /*! 
  *************************************
  * Multiple Items Carousel
@@ -5340,89 +5431,6 @@ theme = ( function ( theme, $, window, document ) {
 
 
 
-
-
-
-/*! 
- *************************************
- * Mousewheel Interaction
- *************************************
- */
-theme = ( function ( theme, $, window, document ) {
-    'use strict';
-   
-   
-    var documentReady = function( $ ){
-		
-		
-		//Prevent this module from loading in other pages
-		if ( !$( 'body' ).hasClass( 'page-mousewheel-eff' ) ) return false;
-		
-		
-	    //Determine the direction of a jQuery scroll event
-		//Fix an issue for mousewheel event is too fast.
-		var mousewheelTrigger = true,
-			scrollCount       = 0;
-		
-		$( window ).on( 'mousewheel', function( event ) { 
-
-			if ( mousewheelTrigger ) {
-
-				if( event.originalEvent.wheelDelta < 0) {
-					//scroll down
-					$( '#demo-mousewheel-interaction-status' ).text( 'Direction: down, Total: ' + scrollCount );
-
-					scrollCount++;
-					
-					//Prohibited scrolling trigger
-					mousewheelTrigger = false;
-					
-					//Do something
-					customMouseHandle();		
-					
-
-				} else {
-					//scroll up
-					$( '#demo-mousewheel-interaction-status' ).text( 'Direction: up, Total: ' + scrollCount );
-
-					scrollCount++;
-					
-					//Prohibited scrolling trigger
-					mousewheelTrigger = false;
-					
-					//Do something
-					customMouseHandle();
-
-				}	
-
-			}
-
-		});
-	
-		
-		
-
-		function customMouseHandle() {
-			
-			//Reset scrolling trigger
-			setTimeout( function() {
-				mousewheelTrigger = true;	
-			}, 1500 );
-			
-			
-		}
-
-		
-	};
-		
-      
-    theme.mousewheelInteraction = {
-        documentReady : documentReady        
-    };  
-    theme.components.documentReady.push( documentReady );
-    return theme;
-
-}( theme, jQuery, window, document ) );
 
 
 /*! 
