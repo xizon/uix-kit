b27b471cab1889c659a9ea12bd65a935aa37d296
UIUX Lab <uiuxlab@gmail.com>
2025-03-21 11:58:09 +0800

    update browser of utils

--- src/components/_global/js/utils/browser.js
+++ src/components/_global/js/utils/browser.js
@@ -11,13 +11,28 @@ try {
   document.addEventListener("test", null, { get passive() { supportsPassive = true }});
 } catch(e) {}
 
+const userAgent = window.navigator.userAgent.toLowerCase();
+const mobileKeywords = [
+    'android',
+    'iphone',
+    'ipad',
+    'ipod',
+    'webos',
+    'blackberry',
+    'windows phone',
+    'opera mini',
+    'mobile',
+    'tablet'
+];
+
 
 export const UixBrowser = {
-    isMobile        : /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
-	isAndroid       : /(android)/i.test(navigator.userAgent),
-	isPC            : !navigator.userAgent.match(/(iPhone|iPod|Android|ios|Mobile)/i),
-	isSafari        : !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/), /*Test to 9, 10. */
-	isIE            : !!window.ActiveXObject || "ActiveXObject" in window,     /*Test to 6 ~ 11 (not edge) */
+    isTablet        : /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(userAgent),
+    isMobile        : mobileKeywords.some(keyword => userAgent.includes(keyword)),
+    isAndroid       : /(android)/i.test(navigator.userAgent),
+    isPC            : !navigator.userAgent.match(/(iPhone|iPod|Android|ios|Mobile)/i),
+    isSafari        : !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/), /*Test to 9, 10. */
+    isIE            : !!window.ActiveXObject || "ActiveXObject" in window,     /*Test to 6 ~ 11 (not edge) */
     supportsPassive : supportsPassive
 };
 
